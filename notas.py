f686970d3fc9583ff65617f93361f57777a6692d
Gustavo Colmenares Zarache <94815835+Gustavo9481@users.noreply.github.com>
2022-07-23 11:10:48 -0500

    Initial commit

---
+++ notas.py
@@ -0,0 +1,44 @@
+# Métodos para Listas °°py-listas
+
+agua = 1
+arena = 2
+cemento = 3
+
+# Métodos de strings °°py-strings
+
+name = "hola gustavo"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# tercer bloque °°py-tuplas
